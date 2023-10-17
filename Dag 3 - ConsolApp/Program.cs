b9e188135bd6da34469ca650f9b61534071e1176
TheEvilness <alexop2000@gmail.com>
2023-10-17 20:35:03 +0200

    End of "looping logic"

--- Dag 3 - ConsolApp/Program.cs
+++ Dag 3 - ConsolApp/Program.cs
@@ -1,29 +1,72 @@
-﻿int playerHealth = 10;
-int monsterHealth = 10;
-int damage = 0;
-Random random = new Random();
-while (playerHealth > 0 && monsterHealth > 0)
+﻿/* Project 1 section.
+while (true)
 {
-	damage = random.Next(1, 10);
-	monsterHealth -= damage;
-	Console.WriteLine("=====================================");
-	Console.WriteLine($"Monster was hit for {damage}");
-	Console.WriteLine($"Current Monster HP: {monsterHealth}");
-	if (monsterHealth > 0)
+	Console.Clear();
+	Console.WriteLine("Input a number between 5 and 10.");
+	Console.Write("Input: ");
+	string? input = Console.ReadLine();
+	int inputAsNumber = -9999;
+	int.TryParse(input, out inputAsNumber);
+	if (inputAsNumber == -9999)
 	{
-		damage = random.Next(1, 10);
-		playerHealth -= damage;
-		Console.WriteLine("=====================================");
-		Console.WriteLine($"Player was hit for {damage}");
-		Console.WriteLine($"Current Player HP: {playerHealth}");
+		Console.WriteLine("Input was not a valid integer.");
+		Console.WriteLine("Press any key to try again.");
+		Console.ReadKey();
+	}
+	if (inputAsNumber > 5 && inputAsNumber < 10)
+	{
+		break;
+	}
+	else
+	{
+		Console.WriteLine("Input needs to be between 5 and 10.");
+		Console.WriteLine("Press any key to try again.");
+		Console.ReadKey();
 	}
 }
-Console.WriteLine("=====================================");
-if (playerHealth > 0)
+Console.WriteLine("Input was valid");
+End project 1 section. */
+
+/*Project 2 section.
+string[] roles = new string[] { "administrator", "manager", "user" };
+string? input = "";
+while (true)
 {
-	Console.WriteLine("Player won!");
+	Console.Clear();
+	Console.WriteLine("Enter your role (Administrator, Manager or User).");
+	Console.Write("Input: ");
+	input = Console.ReadLine();
+	if (roles.Contains(input.ToLower().Trim()))
+	{
+		break;
+	}
+	else
+	{
+		Console.WriteLine($"{input} is not a valid role.");
+		Console.WriteLine("Press any key to try again.");
+		Console.ReadKey();
+	}
 }
-else
+Console.WriteLine($"Role {input} was accepted.");
+End project 2 section. */
+
+/*Project 3 section.
+string[] myStrings = new string[2] { "I like pizza. I like roast chicken. I like salad", "I like all three of the menu choices" };
+int periodLocation = 0;
+foreach (string text in myStrings)
 {
-	Console.WriteLine("Monster won!");
+	string myString = text;
+	periodLocation = myString.IndexOf('.');
+
+	while (periodLocation != -1)
+	{
+		string output = myString.Remove(periodLocation);
+		myString = myString.Substring(periodLocation + 1);
+		myString = myString.TrimStart();
+		periodLocation = myString.IndexOf('.');
+		Console.WriteLine(output);
+	}
+	myString = myString.TrimStart();
+	Console.WriteLine(myString);
 }
+End project 3 section. */
