0d310c77cf71019f2d49bdc215d8a63241b9b2ed
TheEvilness <alexop2000@gmail.com>
2023-10-17 09:20:07 +0200

    End of "Perform basic operations on numbers in C#"

--- Dag 1 - Consol/Program.cs
+++ Dag 1 - Consol/Program.cs
@@ -1,26 +1,7 @@
-﻿Console.WriteLine("Generating invoices for customer \"Contoso Corp\" ...\n");
-Console.WriteLine("Invoice: 1021\t\tComplete!");
-Console.WriteLine("Invoice: 1022\t\tComplete!");
-Console.WriteLine("\nOutput Directory:\t");
+﻿int fahrenheit = 94;
+decimal celcius;
+fahrenheit -= 32;
+decimal ninth = 5m / 9m;
+celcius = fahrenheit * ninth;
 
-Console.WriteLine(@"    c:\source\repos    
-        (this is where your code goes)");
-
-Console.Write(@"c:\invoices");
-
-// Kon'nichiwa World
-Console.WriteLine("\u3053\u3093\u306B\u3061\u306F World!");
-
-// To generate Japanese invoices:
-// Nihon no seikyū-sho o seisei suru ni wa:
-Console.Write("\n\n\u65e5\u672c\u306e\u8acb\u6c42\u66f8\u3092\u751f\u6210\u3059\u308b\u306b\u306f\uff1a\n\t");
-// User command to run an application
-Console.WriteLine(@"c:\invoices\app.exe -j");
-
-
-string projectName = "ACME";
-string russianMessage = "\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434";
-Console.WriteLine("View English output:");
-Console.WriteLine($@"  c:\Exercise\{projectName}\data.txt");
-Console.WriteLine(russianMessage);
-Console.WriteLine($@"  c:\Exercise\{projectName}\ru-RU\data.txt");
+Console.WriteLine($"The temperature is {celcius} Celsius.");
