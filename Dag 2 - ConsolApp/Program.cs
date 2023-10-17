ed089004c91e3f7d440f4a16c914df497d7b40db
TheEvilness <alexop2000@gmail.com>
2023-10-17 12:48:37 +0200

    End of "Call methods from the .NET Class Library using C# "

--- Dag 2 - ConsolApp/Program.cs
+++ Dag 2 - ConsolApp/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+int firstValue = 500;
+int secondValue = 600;
+int maxValue = Math.Max(firstValue, secondValue);
+Console.WriteLine(maxValue);
