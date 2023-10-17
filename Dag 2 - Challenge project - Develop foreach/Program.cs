72419a56ce4b551697fc7493c2b811a5187ba231
TheEvilness <alexop2000@gmail.com>
2023-10-17 18:28:44 +0200

    End of "Develop Foreach"
    I give up, nothing makes sense.

--- Dag 2 - Challenge project - Develop foreach/Program.cs
+++ Dag 2 - Challenge project - Develop foreach/Program.cs
@@ -25,9 +25,17 @@ int examAssignments = 5;
 string[] studentNames = new string[] { "Sophia", "Andrew", "Emma", "Logan" };
 
 int[] sophiaScores = new int[] { 90, 86, 87, 98, 100, 94, 90 };
+decimal sophiaExamScore = 92.2m;
+decimal sophiaCredit = 92m;
 int[] andrewScores = new int[] { 92, 89, 81, 96, 90, 89 };
+decimal andrewExamScore = 89.6m;
+decimal andrewCredit = 89m;
 int[] emmaScores = new int[] { 90, 85, 87, 98, 68, 89, 89, 89 };
+decimal emmaExamScore = 85.6m;
+decimal emmaCredit = 89m;
 int[] loganScores = new int[] { 90, 95, 87, 88, 96, 96 };
+decimal loganExamScore = 91.2m;
+decimal loganCredit = 96m;
 
 int[] studentScores = new int[10];
 
@@ -35,7 +43,7 @@ string currentStudentLetterGrade = "";
 
 // display the header row for scores/grades
 Console.Clear();
-Console.WriteLine("Student\t\tGrade\tLetter Grade\n");
+Console.WriteLine("Student\t\tExam Score\tOverall\tGrade\tExtra Credit\n");
 
 /*
 The outer foreach loop is used to:
@@ -47,88 +55,108 @@ The outer foreach loop is used to:
 */
 foreach (string name in studentNames)
 {
-    string currentStudent = name;
+	decimal examScore = 0m;
+	decimal extraCredits = 0m;
+	string extraPts = "";
+	string currentStudent = name;
 
-    if (currentStudent == "Sophia")
-        studentScores = sophiaScores;
+	if (currentStudent == "Sophia")
+	{
+		studentScores = sophiaScores;
+		examScore = sophiaExamScore;
+		extraCredits = sophiaCredit;
+	}
 
-    else if (currentStudent == "Andrew")
-        studentScores = andrewScores;
+	else if (currentStudent == "Andrew")
+	{
+		studentScores = andrewScores;
+		examScore = andrewExamScore;
+		extraCredits = andrewCredit;
+	}
 
-    else if (currentStudent == "Emma")
-        studentScores = emmaScores;
+	else if (currentStudent == "Emma")
+	{
+		studentScores = emmaScores;
+		examScore = emmaExamScore;
+		extraCredits = emmaCredit;
+	}
 
-    else if (currentStudent == "Logan")
-        studentScores = loganScores;
+	else if (currentStudent == "Logan")
+	{
+		studentScores = loganScores;
+		examScore = loganExamScore;
+		extraCredits = loganCredit;
+	}
 
-    int sumAssignmentScores = 0;
+	int sumAssignmentScores = 0;
 
-    decimal currentStudentGrade = 0;
+	decimal currentStudentGrade = 0;
 
-    int gradedAssignments = 0;
+	int gradedAssignments = 0;
 
-    /* 
+	/* 
     the inner foreach loop sums assignment scores
     extra credit assignments are worth 10% of an exam score
     */
-    foreach (int score in studentScores)
-    {
-        gradedAssignments += 1;
+	foreach (int score in studentScores)
+	{
+		gradedAssignments += 1;
 
-        if (gradedAssignments <= examAssignments)
-            sumAssignmentScores += score;
+		if (gradedAssignments <= examAssignments)
+			sumAssignmentScores += score;
 
-        else
-            sumAssignmentScores += score / 10;
-    }
+		else
+			sumAssignmentScores += score / 10;
+	}
 
-    currentStudentGrade = (decimal)(sumAssignmentScores) / examAssignments;
+	currentStudentGrade = (decimal)(sumAssignmentScores) / examAssignments;
+	extraPts = ((extraCredits * 0.10m) / examAssignments).ToString();
 
-    if (currentStudentGrade >= 97)
-        currentStudentLetterGrade = "A+";
+	if (currentStudentGrade >= 97)
+		currentStudentLetterGrade = "A+";
 
-    else if (currentStudentGrade >= 93)
-        currentStudentLetterGrade = "A";
+	else if (currentStudentGrade >= 93)
+		currentStudentLetterGrade = "A";
 
-    else if (currentStudentGrade >= 90)
-        currentStudentLetterGrade = "A-";
+	else if (currentStudentGrade >= 90)
+		currentStudentLetterGrade = "A-";
 
-    else if (currentStudentGrade >= 87)
-        currentStudentLetterGrade = "B+";
+	else if (currentStudentGrade >= 87)
+		currentStudentLetterGrade = "B+";
 
-    else if (currentStudentGrade >= 83)
-        currentStudentLetterGrade = "B";
+	else if (currentStudentGrade >= 83)
+		currentStudentLetterGrade = "B";
 
-    else if (currentStudentGrade >= 80)
-        currentStudentLetterGrade = "B-";
+	else if (currentStudentGrade >= 80)
+		currentStudentLetterGrade = "B-";
 
-    else if (currentStudentGrade >= 77)
-        currentStudentLetterGrade = "C+";
+	else if (currentStudentGrade >= 77)
+		currentStudentLetterGrade = "C+";
 
-    else if (currentStudentGrade >= 73)
-        currentStudentLetterGrade = "C";
+	else if (currentStudentGrade >= 73)
+		currentStudentLetterGrade = "C";
 
-    else if (currentStudentGrade >= 70)
-        currentStudentLetterGrade = "C-";
+	else if (currentStudentGrade >= 70)
+		currentStudentLetterGrade = "C-";
 
-    else if (currentStudentGrade >= 67)
-        currentStudentLetterGrade = "D+";
+	else if (currentStudentGrade >= 67)
+		currentStudentLetterGrade = "D+";
 
-    else if (currentStudentGrade >= 63)
-        currentStudentLetterGrade = "D";
+	else if (currentStudentGrade >= 63)
+		currentStudentLetterGrade = "D";
 
-    else if (currentStudentGrade >= 60)
-        currentStudentLetterGrade = "D-";
+	else if (currentStudentGrade >= 60)
+		currentStudentLetterGrade = "D-";
 
-    else
-        currentStudentLetterGrade = "F";
+	else
+		currentStudentLetterGrade = "F";
 
-    // Student         Grade
-    // Sophia:         92.2    A-
+	// Student         Grade
+	// Sophia:         92.2    A
 
-    Console.WriteLine($"{currentStudent}\t\t{currentStudentGrade}\t{currentStudentLetterGrade}");
+	Console.WriteLine($"{currentStudent}\t\t{examScore}\t\t{currentStudentGrade}\t{currentStudentLetterGrade}\t{extraCredits} ({extraPts})");
 }
 
 // required for running in VS Code (keeps the Output windows open to view results)
 Console.WriteLine("\n\rPress the Enter key to continue");
-Console.ReadLine();
+Console.ReadKey();
