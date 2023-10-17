c7bbafded389ae9543cf8e81e151fd1e4da595b7
TheEvilness <alexop2000@gmail.com>
2023-10-17 19:51:42 +0200

    End of "Add looping logic"

--- Dag 3 - ConsolApp/Program.cs
+++ Dag 3 - ConsolApp/Program.cs
@@ -1,13 +1,29 @@
-﻿for (int i = 1; i < 101; i++)
+﻿int playerHealth = 10;
+int monsterHealth = 10;
+int damage = 0;
+Random random = new Random();
+while (playerHealth > 0 && monsterHealth > 0)
 {
-	string message = "";
-	if (i % 3 == 0)
+	damage = random.Next(1, 10);
+	monsterHealth -= damage;
+	Console.WriteLine("=====================================");
+	Console.WriteLine($"Monster was hit for {damage}");
+	Console.WriteLine($"Current Monster HP: {monsterHealth}");
+	if (monsterHealth > 0)
 	{
-		message += "Fizz";
+		damage = random.Next(1, 10);
+		playerHealth -= damage;
+		Console.WriteLine("=====================================");
+		Console.WriteLine($"Player was hit for {damage}");
+		Console.WriteLine($"Current Player HP: {playerHealth}");
 	}
-	if (i % 5 == 0)
-	{
-		message += "Buzz";
-	}
-	Console.WriteLine(i + " " + message);
+}
+Console.WriteLine("=====================================");
+if (playerHealth > 0)
+{
+	Console.WriteLine("Player won!");
+}
+else
+{
+	Console.WriteLine("Monster won!");
 }
