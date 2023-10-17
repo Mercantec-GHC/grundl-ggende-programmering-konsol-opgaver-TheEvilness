84f14458b77925e20bbe1215a8984c21bd298add
TheEvilness <alexop2000@gmail.com>
2023-10-17 13:09:06 +0200

    End of "Decision Logic"

--- Dag 2 - ConsolApp/Program.cs
+++ Dag 2 - ConsolApp/Program.cs
@@ -1,5 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-int firstValue = 500;
-int secondValue = 600;
-int maxValue = Math.Max(firstValue, secondValue);
-Console.WriteLine(maxValue);
+﻿Random random = new Random();
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
+
+if (daysUntilExpiration == 0)
+{
+	Console.WriteLine("Your subscription has expired.");
+}
+else if (daysUntilExpiration == 1)
+{
+	Console.WriteLine("Your subscription expires within a day!");
+	Console.WriteLine("Renew now and save 20%!");
+}
+else if (daysUntilExpiration <= 5)
+{
+	Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.");
+	Console.WriteLine("Renew now and save 10%!");
+}
+else if (daysUntilExpiration <= 10)
+{
+	Console.WriteLine("Your subscription will expire soon. Renew now!");
+}
