1fd98fbcbbe26708f2abfbe4a0b9f910db963e80
TheEvilness <alexop2000@gmail.com>
2023-10-18 10:48:22 +0200

    End of "Convert data types"

--- Dag 4.1 - ConsoleApp/Program.cs
+++ Dag 4.1 - ConsoleApp/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] values = { "12,3", "45", "ABC", "11", "DEF" };
+
+string message = "";
+decimal total = 0m;
+foreach (string value in values)
+{
+	if (decimal.TryParse(value, out decimal result))
+	{
+		total += result;
+	}
+	else
+	{
+		message += value;
+	}
+}
+Console.WriteLine($"Message: {message}");
+Console.WriteLine($"Total: {total}");
