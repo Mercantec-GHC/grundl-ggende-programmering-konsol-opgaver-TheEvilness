a9d8cc092bb54996278a5df47a00a0957cf9f1b8
TheEvilness <alexop2000@gmail.com>
2023-10-18 11:39:18 +0200

    End of "Format alphanumeric data"

--- Dag 4.1 - ConsoleApp/Program.cs
+++ Dag 4.1 - ConsoleApp/Program.cs
@@ -14,6 +14,7 @@ foreach (string word in reversedWords)
 	Console.Write($"{word} ");
 }*/
 
+/* Second exercise
 string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
 List<string> split = orderStream.Split(',').ToList();
 split.Sort();
@@ -26,4 +27,35 @@ foreach (string orderid in split)
 		error = "\t- Error";
 	}
 	Console.WriteLine($"{orderid}{error}");
-}
+}*/
+
+string customerName = "Ms. Barros";
+
+string currentProduct = "Magic Yield";
+int currentShares = 2975000;
+decimal currentReturn = 0.1275m;
+decimal currentProfit = 55000000.0m;
+
+string newProduct = "Glorious Future";
+decimal newReturn = 0.13125m;
+decimal newProfit = 63000000.0m;
+
+// Your logic here
+Console.WriteLine("Dear Ms. Barros,");
+Console.WriteLine($"As a customer of our {currentProduct} offering we are excited to tell you about a new financial product that would dramatically increase your return.\r\n");
+Console.WriteLine($"Currently, you own {currentShares:N} shares at a return of {currentProduct:P}.\r\n");
+Console.WriteLine($"Our new product, {newProduct} offers a return of {newReturn:P}.  Given your current volume, your potential profit would be {newProfit:C}.\r\n");
+Console.WriteLine("Here's a quick comparison:\n");
+string comparisonMessage = "";
+comparisonMessage = currentProduct.PadRight(20);
+comparisonMessage += String.Format("{0:P}", currentReturn).PadRight(10);
+comparisonMessage += String.Format("{0:C}", currentProfit).PadRight(20);
+
+comparisonMessage += "\n";
+comparisonMessage += newProduct.PadRight(20);
+comparisonMessage += String.Format("{0:P}", newReturn).PadRight(10);
+comparisonMessage += String.Format("{0:C}", newProfit).PadRight(20);
+
+// Your logic here
+
+Console.WriteLine(comparisonMessage);
